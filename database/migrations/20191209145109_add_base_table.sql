0955ce5108af42557d30a64e34d7b23afc21e261
Camilo <cagodoy@acid.cl>
2019-12-17 06:31:20 -0300

    fix migration

--- database/migrations/20191209145109_add_base_table.sql
+++ database/migrations/20191209145109_add_base_table.sql
@@ -2,9 +2,9 @@
 -- +goose StatementBegin
 CREATE TABLE history (
   id uuid primary key default gen_random_uuid() UNIQUE,
-  user_id varchar(255)  NOT NULL UNIQUE,
-  latitude varchar(255)  NOT NULL UNIQUE,
-  longitude varchar(255)  NOT NULL UNIQUE,
+  user_id varchar(255)  NOT NULL,
+  latitude varchar(255)  NOT NULL,
+  longitude varchar(255)  NOT NULL,
   created_at timestamptz default now(),
   updated_at timestamptz default now(),
   deleted_at timestamptz
